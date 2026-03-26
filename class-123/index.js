19f4df4370127c7efc0b7403e6753d41e2c21e75
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-03-26 19:30:53 +0530

    done

--- class-123/index.js
+++ class-123/index.js
@@ -1,8 +1,19 @@
 import "dotenv/config";
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
-import { sendEmail } from "./Mail.Service";
+import { createAgent, HumanMessage, tool } from "langchain";
+import { sendEmail } from "./Mail.Service.js";
+import * as z from "zod";
+
+const emailTool = tool(sendEmail, {
+  name: "emailTool",
+  description: "Use this tool to send an email",
+  schema: z.object({
+    to: z.string().describe("The recipient's email address"),
+    html: z.string().describe("The HTML content of the email"),
+    subject: z.string().describe("The subject of the email"),
+  }),
+});
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -13,14 +24,23 @@ const model = new ChatMistralAI({
   model: "mistral-small-latest",
 });
 
-const message = [];
+const agent = createAgent({
+  model,
+  tools: [emailTool],
+});
+
+const messages = [];
 
 while (true) {
-  const userInput = await rl.question("You: ");
-  message.push(new HumanMessage(userInput))
-  const response = await model.invoke(message);
-  message.push(response)
-  console.log("AI: " + response.content);
+  const userInput = await rl.question("\x1b[32mYou:\x1b[0m ");
+  messages.push(new HumanMessage(userInput));
+
+  const response = await agent.invoke({ messages });
+  // messages.push(response);
+  messages.push(response.messages[response.messages.length - 1]);
+
+  // console.log(response)
+  console.log(`\x1b[34m[AI]\x1b[0m ${response.messages[response.messages.length - 1].content}`,);
 }
 
-rl.close();
+
