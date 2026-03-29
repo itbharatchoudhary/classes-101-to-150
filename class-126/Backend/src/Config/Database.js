578b0abf14d7b9ee995072b46f36791504c0a1eb
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-03-29 17:36:45 +0530

    done

--- class-126/Backend/src/Config/Database.js
+++ class-126/Backend/src/Config/Database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected Successfully`);
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
 };
 
 export default connectDB;
